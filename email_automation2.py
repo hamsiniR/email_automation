0ae0f50bcd487fe12963748e987a5c768b664f1f
hamsiniR <32734366+hamsiniR@users.noreply.github.com>
2020-08-17 17:31:13 +0530

    Update email_automation2.py

--- email_automation2.py
+++ email_automation2.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug 17 12:22:51 2020
 
-@author: HP
-"""
 import os
 import smtplib
 from email.message import EmailMessage
@@ -11,30 +6,30 @@ import imghdr
 
 email=os.environ.get('email')
 password=os.environ.get('pass')
-print(email)
-print(password)
 
+#To send to multiple contacts
 contacts=[email,'hamsinishreyas3598@gmail.com']
 
+# instaed of using fstring for the content of EmailMessage package has been used
 msg=EmailMessage()
 msg['Subject']='My test email'
 msg['From']=email
-#msg['To']=email
 msg['To']=contacts
 msg.set_content("yayy")
 
-
+# To attach an image
 with open(r"C:\Users\HP\Desktop\pic_dog.jpg","rb") as f:
     file_data=f.read()
     file_type=imghdr.what(f.name)
     file_name=f.name
 msg.add_attachment(file_data,maintype='image',subtype=file_type,filename=file_name)
 
+# to attatch a pdf
 with open(r"C:\Users\HP\Desktop\Hamsini Ravishankar-Resume.pdf","rb") as f:
     file_data=f.read()
     file_name=f.name
 msg.add_attachment(file_data,maintype='application',subtype='octet-stream',filename=file_name)
-
+#SMTP_SSL to directly have a SSL connection
 with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
    
     
