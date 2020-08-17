a77af0a2721680ea066fe63df238a7887ef908cd
hamsiniR <32734366+hamsiniR@users.noreply.github.com>
2020-08-17 17:16:22 +0530

    Update email_automation.py

--- email_automation.py
+++ email_automation.py
@@ -1,30 +1,26 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Aug 14 13:22:52 2020
 
-@author: HP
-"""
 
 import os
 import smtplib
-
+# sensitive values have been put as environmental variables
 email=os.environ.get('email')
 password=os.environ.get('pass')
-print(email)
-print(password)
 
+#We are using the SMTP class to connect to a gmail account
 with smtplib.SMTP('smtp.gmail.com',587) as smtp:
+    # ehlo() to ge
     smtp.ehlo()
     smtp.starttls()
     smtp.ehlo()
     
-   
+    #logging in to email
     smtp.login(email,password)
     
     subject="My first Email via python"
     body="yayy"
-    
+    # here we are using  fsting for the subject and body of our email
     msg=f'Subject : {subject}\n\n{body}'
    
+    #The first attribue is sender email while the second attribute is receiver email
     smtp.sendmail(email,email,msg)
     print('email sent')
